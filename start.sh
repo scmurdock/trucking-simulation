666e98eb226d4b84db9cc422e3800955497a9f7c
Sean Murdock <scmurdock@gmail.com>
2022-06-02 07:28:37 -0600

    Update start script

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-java -Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n -Dconfig=/etc/stedi/application.conf -jar target/StepTimerWebsocket-1.0-SNAPSHOT.jar
+java -Xdebug -Xrunjdwp:transport=dt_socket,address=8001,server=y,suspend=n -Dconfig=/etc/trucking-simulation/application.conf -jar /opt/trucking-simulation/TruckingSimulation-1.0-SNAPSHOT.jar
