f9b4b2aeaf8d62edc364992b02a85941b1e0e781
Sean Murdock <scmurdock@gmail.com>
2022-06-09 06:22:30 -0600

    Updated script

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
-git-force-clone --branch data-engineering-nanodegree https://github.com/scmurdock/trucking-simulation.git /opt/trucking-simulation
+cd /opt/trucking-simulation
+git pull
 java -Xdebug -Xrunjdwp:transport=dt_socket,address=8001,server=y,suspend=n -Dconfig=/etc/trucking-simulation/application.conf -jar /opt/trucking-simulation/TruckingSimulation-1.0-SNAPSHOT.jar
