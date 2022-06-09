91bb9410f38b698ddc47eece0c5d9a24291ad34f
Sean Murdock <scmurdock@gmail.com>
2022-06-09 06:49:36 -0600

    Added comments to script

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
+# First change to the directory where your repository was cloned to begin with
 cd /opt/trucking-simulation
+# Then pull the latest
 git pull
+# Then run the jar
 java -Xdebug -Xrunjdwp:transport=dt_socket,address=8001,server=y,suspend=n -Dconfig=/etc/trucking-simulation/application.conf -jar /opt/trucking-simulation/TruckingSimulation-1.0-SNAPSHOT.jar
